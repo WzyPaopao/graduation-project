066c9cedca858490cd8d27ad793c6e48d2fc55c7
王正奕 <alan@wangzhengyis-MacBook-Pro.local>
2021-09-24 17:28:27 +0800

    correct the gui api usage

--- ui/gui_api_test.py
+++ ui/gui_api_test.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+from tkinter import *
 
-root = tk.Tk()
+root = Tk()
 
 
 def callback():
@@ -8,12 +8,24 @@ def callback():
 
 
 # 创建一个顶级菜单
-menubar = tk.Menu(root)
-menubar.add_command(label="Hello", command=callback)
-menubar.add_command(label="Quit", command=root.quit)
+menu_bar = Menu(root)
+
+file_menu = Menu(menu_bar)
+file_menu.add_command(label="Hello", command=callback)
+file_menu.add_command(label="Quit", command=root.quit)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+connect_bar = Menu(menu_bar)
+connect_bar.add_command(label="Detect Online")
+connect_bar.add_command(label="Preference")
+menu_bar.add_cascade(label="Connect", menu=connect_bar)
+
+help_bar = Menu(menu_bar)
+help_bar.add_command(label="About")
+help_bar.add_command(label="Help")
+menu_bar.add_cascade(label="About", menu=help_bar)
 
 # 显示菜单
-root.config(menu=menubar)
+root.config(menu=menu_bar)
 
 root.mainloop()
-
