919554c931a045c59d29c3fd46c443ad6f76ec5e
王正奕 <alan@wangzhengyis-MacBook-Pro.local>
2021-09-24 16:50:47 +0800

    add gui_api_test.py file & design the main ui

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -1,28 +1,43 @@
-from tkinter import Tk, Listbox, Button, Label, messagebox
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+import hashlib
+import time
+
+LOG_LINE_NUM = 0
 
 
-root = Tk()  # 创建窗口对象的背景色
-# 创建两个列表
-li = ['C', 'python', 'php', 'html', 'SQL', 'java']
-movie = ['CSS', 'jQuery', 'Bootstrap']
-listb = Listbox(root)  # 创建两个列表组件
-listb2 = Listbox(root)
-for item in li:  # 第一个小部件插入数据
-    listb.insert(0, item)
+class MY_GUI:
+    def __init__(self, init_window_name):
+        self.init_window_name = init_window_name
 
-for item in movie:  # 第二个小部件插入数据
-    listb2.insert(0, item)
+    # 设置窗口
+    def set_init_window(self):
+        self.init_window_name.title("三位目标识别工具")  # 窗口名
+        self.init_window_name.geometry('1068x681+10+10')
+
+        self.menu_btn_file = Menu(self.init_window_name, tearoff=0)
+        self.menu_btn_file.add_command(label='打开场景', command=self.hello)
+        self.menu_btn_file.add_separator()
+        self.menu_btn_file.add_command(label='退出', command=self.init_window_name.quit)
+        self.init_window_name.config(menu=self.menu_btn_file)
+
+    def hello(self):
+        print("hello world")
 
 
-def hello_call_back():
-   messagebox.showinfo("Hello Python", "Hello Runoob")
 
 
-btn = Button(root, text="click me", command=hello_call_back)
-lb = Label(root, text="hello world")
+def gui_start():
+    init_window = Tk()  # 实例化出一个父窗口
+    MAIN_WIN = MY_GUI(init_window)
 
-listb.pack()  # 将小部件放置到主窗口中
-listb2.pack()
-btn.pack()
-lb.pack()
-root.mainloop()  # 进入消息循环
+    # 设置根窗口默认属性
+    MAIN_WIN.set_init_window()
+
+    init_window.mainloop()  # 父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
+
+
+if __name__ == '__main__':
+    gui_start()
