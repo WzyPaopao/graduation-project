f4fc65d2790a539e6c29b60f28b571f83993ec24
alan <coder_wzy@163.com>
2021-11-05 18:52:31 +0800

    Revert "modify ignore file"
    This reverts commit 4d954e21fcd68c86501410e43833259723853322.

--- pyqt_learn/run_test_1.py
+++ pyqt_learn/run_test_1.py
@@ -1,12 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget
 from test_1 import Ui_MainWindow
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_window = QMainWindow()
+    # print("hello")
+    # # 创建QApplication实例
+    # app = QApplication(sys.argv)
+    # # 创建一个窗口
+    # w = QWidget()
+    # # 设定窗口尺寸
+    # w.resize(300, 150)
+    # # 移动窗口
+    # w.move(300, 300)
+    # # 设置窗口属性
+    # w.setWindowTitle("hello world")
+    # # 显示窗口
+    # w.show()
+    #
+    # # 进入程序的主循环，并通过exit确保主循环安全结束
+    # sys.exit(app.exec_())
     ui = Ui_MainWindow()
-    ui.setupUi(main_window)
-    main_window.show()
-    sys.exit(app.exec_())
+    ui.setupUi()
