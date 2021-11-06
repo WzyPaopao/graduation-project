aae47f20c2559c7490c7c71e4d5f463fee75fa89
alan <coder_wzy@163.com>
2021-11-06 16:31:18 +0800

    add absolute ui

---
+++ pyqt_learn/RunMainWinFormLayout.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.Qt import QApplication, QMainWindow
+import MainWinFormLayout
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = MainWinFormLayout.Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
