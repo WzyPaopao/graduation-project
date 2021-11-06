0ee34aff691275bde1217dfc5c0ae7bbc0ec8280
alan <coder_wzy@163.com>
2021-11-06 18:34:24 +0800

    add size policy ui

--- pyqt_learn/MainWinSpacerLine.py
+++ pyqt_learn/MainWinSpacerLine.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'MainWinSpacerLine.ui'
+# Form implementation generated from reading ui file 'MainWinSizePolicyDemo.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
---
+++ pyqt_learn/RunMainWinSizePolicy.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.Qt import QApplication, QMainWindow
+import MainWinSizePolicy
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = MainWinSizePolicy.Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
