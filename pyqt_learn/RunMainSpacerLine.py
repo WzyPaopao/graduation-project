511d1a68b74c6296499c682ff15ffa9a833567b6
alan <coder_wzy@163.com>
2021-11-06 16:39:15 +0800

    add spacer line ui

--- pyqt_learn/RunMainSpacerLine.py
+++ pyqt_learn/RunMainSpacerLine.py
@@ -1,12 +1,12 @@
 import sys
-import MainWinContainerLayout
+import MainWinSpacerLine
 from PyQt5.Qt import QApplication, QMainWindow
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = QMainWindow()
-    ui = MainWinContainerLayout.Ui_MainWindow()
+    ui = MainWinSpacerLine.Ui_MainWindow()
     ui.setupUi(main_window)
     main_window.show()
     sys.exit(app.exec_())
