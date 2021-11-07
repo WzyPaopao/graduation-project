aaeb4840e15b8188ec8dd89760ad92c9bf16c2c5
alan <coder_wzy@163.com>
2021-11-07 13:57:31 +0800

    add toolbar & signalslot ui

---
+++ pyqt_learn/RunMainSignalSlot.py
@@ -0,0 +1,12 @@
+import sys
+import MainWinSignalSlot
+from PyQt5.Qt import QApplication, QMainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = MainWinSignalSlot.Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
