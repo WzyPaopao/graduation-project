17a81d48826054737b6ebdf15aec4ee43a0824e6
alan <coder_wzy@163.com>
2021-11-14 13:16:06 +0800

    learn QLineEdit echo mode

---
+++ pyqt_learn/controls/QLineEditEchoMode.py
@@ -0,0 +1,64 @@
+"""
+QLineEdit控件与回显模式
+
+基本功能：输入单行文本
+高级使用：设置回显模式 EchoMode
+
+EchoMode（回显模式）四种回显模式：
+- Normal  正常
+- NoEcho  不回显
+- Password  密文回显
+- PasswordEchoOnEdit 先回显然后密文
+"""
+
+
+from PyQt5.QtWidgets import *
+import sys
+
+
+class QLineEditEchoMode(QWidget):
+    def __init__(self):
+        super(QLineEditEchoMode, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("文本输入框的回显模式")
+
+        formLayout = QFormLayout()
+
+        normalLineEdit = QLineEdit()
+        noEchoLineEdit = QLineEdit()
+        passwordLineEdit = QLineEdit()
+        passwordEchoOnEditLineEdit = QLineEdit()
+
+        formLayout.addRow("Normal", normalLineEdit)
+        formLayout.addRow("NoEcho", noEchoLineEdit)
+        formLayout.addRow("Password", passwordLineEdit)
+        formLayout.addRow("PasswordEchoOnEdit", passwordEchoOnEditLineEdit)
+
+        # placeholderText
+        normalLineEdit.setPlaceholderText("Normal")
+        noEchoLineEdit.setPlaceholderText("NoEcho")
+        passwordLineEdit.setPlaceholderText("Password")
+        passwordEchoOnEditLineEdit.setPlaceholderText("PasswordEchoOnEdit")
+
+        normalLineEdit.setEchoMode(QLineEdit.Normal)
+        noEchoLineEdit.setEchoMode(QLineEdit.NoEcho)
+        passwordLineEdit.setEchoMode(QLineEdit.Password)
+        passwordEchoOnEditLineEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+
+        self.setLayout(formLayout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QLineEditEchoMode()
+    main.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
