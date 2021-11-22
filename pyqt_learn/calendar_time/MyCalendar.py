eb4c2096205e2dbe774dd13dbc90262274c2592f
Zhengyi <coder_wzy@163.com>
2021-11-22 10:44:57 +0800

    calendar usage

---
+++ pyqt_learn/calendar_time/MyCalendar.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class MyCalendar(QWidget):
+    def __init__(self):
+        super(MyCalendar, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(400, 350)
+        self.cal = QCalendarWidget(self)
+        self.cal.setMinimumDate(QDate(1988, 1, 1))
+        self.cal.setMaximumDate(QDate(2088, 12, 1))
+
+        self.cal.setGridVisible(False)
+        self.cal.move(20, 20)
+        self.cal.clicked.connect(self.showDate)
+
+        self.label = QLabel(self)
+        date = self.cal.selectedDate()
+        self.label.setText(date.toString("yyy-MM-dd dddd"))
+        self.label.move(50, 300)
+
+        self.setWindowTitle("日历演示")
+
+    def showDate(self, date):
+        self.label.setText(self.cal.selectedDate().toString("yyyy-MM-dd dddd"))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MyCalendar()
+    main.show()
+    sys.exit(app.exec_())
