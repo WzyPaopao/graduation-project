6cdc092e034b4ceac330e7120b81c4b743069c73
Zhengyi <coder_wzy@163.com>
2021-11-22 11:21:01 +0800

    learn DateTimeEdit

---
+++ pyqt_learn/calendar_time/DateTimeEdit.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
+class DateTimeEdit(QWidget):
+    def __init__(self):
+        super(DateTimeEdit, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(300, 90)
+        self.setWindowTitle("设置不同风格的日期和时间")
+
+        layout = QVBoxLayout()
+        dateTimeEdit1 = QDateTimeEdit()
+        dateTimeEdit2 = QDateTimeEdit(QDateTime.currentDateTime())
+
+        dateEdit = QDateTimeEdit(QDate.currentDate())
+        timeEdit = QDateTimeEdit(QTime.currentTime())
+
+        dateTimeEdit1.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
+        dateTimeEdit2.setDisplayFormat("yyyy/MM/dd HH-mm-ss")
+        dateEdit.setDisplayFormat("yyyy.MM.dd")
+        timeEdit.setDisplayFormat("HH:mm:ss")
+
+        layout.addWidget(dateTimeEdit1)
+        layout.addWidget(dateTimeEdit2)
+        layout.addWidget(dateEdit)
+        layout.addWidget(timeEdit)
+
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = DateTimeEdit()
+    main.show()
+    sys.exit(app.exec_())
+
