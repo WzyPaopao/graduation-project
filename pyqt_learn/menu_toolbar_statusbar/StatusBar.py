647d74aecd7711f9ea0a3db93fc9829a1bba0e64
Zhengyi <coder_wzy@163.com>
2021-11-24 11:34:38 +0800

    learn status bar

---
+++ pyqt_learn/menu_toolbar_statusbar/StatusBar.py
@@ -0,0 +1,40 @@
+'''
+创建和使用状态栏
+'''
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class StatusBar(QMainWindow):
+    def __init__(self):
+        super(StatusBar, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('状态栏演示')
+        self.resize(300, 200)
+
+        bar = self.menuBar()
+        file = bar.addMenu("File")  # 顶层菜单
+        file.addAction("show")
+        file.triggered.connect(self.processTrigger)
+
+        self.setCentralWidget(QTextEdit())
+
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+
+    def processTrigger(self, q):
+        if q.text() == 'show':
+            self.statusBar.showMessage(q.text() + " 菜单被点击了")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = StatusBar()
+    main.show()
+    sys.exit(app.exec_())
